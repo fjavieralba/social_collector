c2681047510696432cd79e2daa5fb4eb3bc2921f
Javier Alba <me@fjavieralba.com>
2013-12-16 10:33:01 +0100

    added settings

---
+++ retrieval/social_collector/social_collector/settings.py
@@ -0,0 +1,12 @@
+oauth = {
+    #application data:
+    'consumer_key': "",
+    'consumer_secret': "",
+    #user data:
+    'access_token': "",
+    'access_token_secret': ""
+}
+
+rabbitmq = {
+    'host': 'localhost'
+}
