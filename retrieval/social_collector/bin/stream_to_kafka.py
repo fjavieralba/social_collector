32b320a679ddbdc1612f9be1427099a4eaa39314
Javier Alba <me@fjavieralba.com>
2014-03-27 09:48:09 +0100

    added script for stream tweets to kafka topic

---
+++ retrieval/social_collector/bin/stream_to_kafka.py
@@ -0,0 +1,32 @@
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer, KeyedProducer
+import argparse
+from social_collector import twitter, settings
+
+def produce(broker_list, topic, keywords):
+    tw = twitter.Twitter(**settings.oauth)
+    kafka = KafkaClient(broker_list)
+    producer = SimpleProducer(kafka)
+
+	#send messages synchronously
+    for raw_tweet in tw.stream(keywords):
+		producer.send_messages(topic, raw_tweet)
+		
+    kafka.close()
+
+
+#example:
+#python bin/stream_to_kafka.py --broker-list localhost:9092 --topic tweets --keywords "bancosantander,santander_es,santander_br,SantanderMx,santanderchile,FBBancoSantander,bsch,central hispano,banco santander,santanderGP,SacSantander_br,santanderukhelp,santanderuk,santanderpb,empleosantander"
+
+if __name__ == "__main__":
+    #parse args
+    parser = argparse.ArgumentParser(description='Publish a realtime twitter stream into Kafka')
+    parser.add_argument("--broker-list", dest="broker_list", help="comma separated list of kafka brokers", default=settings.kafka['broker_list'])
+    parser.add_argument("--topic", dest="topic", help="kafka topic", default=settings.kafka['topic'])
+    parser.add_argument('--keywords', dest="keywords", help='Keywords to use for filtering Twitter stream', nargs='+')
+
+    args = parser.parse_args()
+
+    produce(args.broker_list, args.topic, args.keywords)
+
+
